1f2d597d51cb311561bbbe223e7edc97854e68b3
Takayuki Yoshioka <yoshioka@farend.jp>
2020-01-16 15:48:42 +0900

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require 'redmine_s3'
 
 Redmine::Plugin.register :redmine_s3 do
-  requires_redmine version_or_higher: '4.1.0'
+  # requires_redmine version_or_higher: '4.1.0'
   name 'S3'
   author 'Chris Dell'
   description 'Use Amazon S3 as a storage engine for attachments'
